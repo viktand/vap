6209986c045c95a5df55b1669cceae6cce7ffded
viktand <viktand@bk.ru>
2013-03-17 20:40:27 +1100

    mar_17

---
+++ about.pas
@@ -0,0 +1,32 @@
+unit about;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+
+  { TForm5 }
+
+  TForm5 = class(TForm)
+    Image1: TImage;
+    StaticText1: TStaticText;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  Form5: TForm5; 
+
+implementation
+
+{$R *.lfm}
+
+end.
+
